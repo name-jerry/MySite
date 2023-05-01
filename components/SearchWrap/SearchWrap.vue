d044fca7c45499fb11ca872c9e517b3d5ec51ec6
name-jerry <413398334@qq.com>
2023-05-01 09:12:14 +0800

    目录层级改为3

--- components/SearchWrap/SearchWrap.vue
+++ components/SearchWrap/SearchWrap.vue
@@ -1,204 +1,229 @@
 <template>
-  <view class="search-wrap">
-    <view class="search-box hover-shadow-input" :class="{'blank':isBlank}">
-      <input type="text" class="search" confirm-type="search" @confirm="enter" v-model='searchWord' @blur="blur"
-        @focus="focusInput" />
-      <Options class="options" :class="{show:showOptions}" :options='optionList' :show-mask='false' @select='select'>
-      </Options>
-      <picker @cancel="shrink=true" @change="pickerChange" @tap="pickerTap" mode="selector" :range='toolList'
-        range-key="label" class="tools">
-        <text>{{toolList[index].label}}</text>
-      </picker>
-      <a ref='btn' :href='toolList[index].value+searchWord' @tap='search' target='_blank' class="btn hover-shadow-btn">
-        <text class="icon-search ">&#xe648;</text>
-      </a>
-    </view>
-  </view>
+	<view class="search-wrap">
+		<view class="search-box hover-shadow-input" :class="{'blank':isBlank}">
+			<input type="text" class="search" confirm-type="search" @confirm="enter" v-model='searchWord' @blur="blur"
+				@focus="focusInput" />
+			<Options class="options" :class="{show:showOptions}" :options='optionList' :show-mask='false'
+				@select='select'>
+			</Options>
+			<picker @cancel="shrink=true" @change="pickerChange" @tap="pickerTap" mode="selector" :range='toolList'
+				range-key="label" class="tools">
+				<text>{{toolList[index].label}}</text>
+			</picker>
+			<a ref='btn' :href='toolList[index].value+searchWord' @tap='search' target='_blank'
+				class="btn hover-shadow-btn">
+				<text class="icon-search ">&#xe648;</text>
+			</a>
+		</view>
+	</view>
 </template>
 
 <script setup lang='ts'>
-  import { ref, reactive, onMounted, onUnmounted, watch, computed } from "vue";
-  import useMainStore from "@/stores/useMainStore"
-  const main = useMainStore()
-  let prop = defineProps < {
-    autoShrink ? : boolean
-  } > ()
-  let shrink = ref < boolean > (true)
-  const optionsKey = 'optionsKey'
-  let toolList = reactive < { label: string, value: string } [] > ([
-    { label: 'Can I Use', value: 'https://caniuse.com/?search=' },
-    { label: 'bing', value: 'https://cn.bing.com/search?q=' },
-    { label: '菜鸟教学', value: 'https://www.runoob.com/?s=' },
-    { label: 'iconfont', value: 'https://www.iconfont.cn/search/index?searchType=icon&q=' },
-  ])
-  let searchWord = ref < string > ()
-  let index = ref < number > (0)
-  let optionList = ref < string[] > ([]);
-  let showOptions = ref < boolean > (false);
-  let isBlank = computed(() => !searchWord.value && prop.autoShrink && shrink.value)
-  let btn = ref()
+	import {
+		ref,
+		reactive,
+		onMounted,
+		onUnmounted,
+		watch,
+		computed
+	} from "vue";
+	import useMainStore from "@/stores/useMainStore"
+	const main = useMainStore()
+	let prop = defineProps < {
+		autoShrink ? : boolean
+	} > ()
+	let shrink = ref < boolean > (true)
+	const optionsKey = 'optionsKey'
+	let toolList = reactive < {
+		label: string,
+		value: string
+	} [] > ([{
+			label: 'bing',
+			value: 'https://cn.bing.com/search?q='
+		},
+		{
+			label: 'bilibili',
+			value: 'https://search.bilibili.com/all?keyword='
+		},
+		{
+			label: 'Can I Use',
+			value: 'https://caniuse.com/?search='
+		},
+		{
+			label: '菜鸟教学',
+			value: 'https://www.runoob.com/?s='
+		},
+	])
+	let searchWord = ref < string > ()
+	let index = ref < number > (0)
+	let optionList = ref < string[] > ([]);
+	let showOptions = ref < boolean > (false);
+	let isBlank = computed(() => !searchWord.value && prop.autoShrink && shrink.value)
+	let btn = ref()
 
-  function pickerChange(e: CustomEvent): void {
-    index.value = e.detail.value;
-    shrink.value = true
-  }
+	function pickerChange(e: CustomEvent): void {
+		index.value = e.detail.value;
+		shrink.value = true
+	}
 
-  function pickerTap(): void {
-    shrink.value = false
-  }
+	function pickerTap(): void {
+		shrink.value = false
+	}
 
-  function focusInput() {
-    showOptions.value = true
-  }
+	function focusInput() {
+		showOptions.value = true
+	}
 
-  function enter(e) {
-    btn.value.click()
-  }
+	function enter(e) {
+		btn.value.click()
+	}
 
-  function blur() {
-    showOptions.value = false
-  }
+	function blur() {
+		showOptions.value = false
+	}
 
-  function search(e: CustomEvent): void {
-    if (isBlank.value) {
-      e.preventDefault();
-      uni.showToast({
-        title: '输入文字',
-        icon: "none"
-      })
-      return
-    }
-    let v = searchWord.value.replace(/(^\s+)|(\s+$)/g, '');
-    if (v == '') return;
-    addOption(v);
-  }
+	function search(e: CustomEvent): void {
+		if (isBlank.value) {
+			e.preventDefault();
+			uni.showToast({
+				title: '输入文字',
+				icon: "none"
+			})
+			return
+		}
+		let v = searchWord.value.replace(/(^\s+)|(\s+$)/g, '');
+		if (v == '') return;
+		addOption(v);
+	}
 
-  function select(item: string): void {
-    searchWord.value = item
-  }
+	function select(item: string): void {
+		searchWord.value = item
+	}
 
-  function setOptions(o: string[]): void {
-    let set = new Set([...o])
-    optionList.value = [...set]
-    uni.setStorageSync(optionsKey, JSON.stringify(optionList.value))
-  }
+	function setOptions(o: string[]): void {
+		let set = new Set([...o])
+		optionList.value = [...set]
+		uni.setStorageSync(optionsKey, JSON.stringify(optionList.value))
+	}
 
-  function addOption(o: string) {
-    let set = new Set([...optionList.value])
-    set.add(o)
-    optionList.value = [...set]
-    uni.setStorageSync(optionsKey, JSON.stringify(optionList.value))
-  }
-  async function refresh() {
-    if (!main.isLogin) return optionList.value = [];
-    let l = uni.getStorageSync(optionsKey);
-    if (l) return setOptions(JSON.parse(l))
-  }
-  onMounted(() => {
-    watch(() => main.isLogin, refresh, { immediate: true })
-  });
-  onUnmounted(() => {})
+	function addOption(o: string) {
+		let set = new Set([...optionList.value])
+		set.add(o)
+		optionList.value = [...set]
+		uni.setStorageSync(optionsKey, JSON.stringify(optionList.value))
+	}
+	async function refresh() {
+		if (!main.isLogin) return optionList.value = [];
+		let l = uni.getStorageSync(optionsKey);
+		if (l) return setOptions(JSON.parse(l))
+	}
+	onMounted(() => {
+		watch(() => main.isLogin, refresh, {
+			immediate: true
+		})
+	});
+	onUnmounted(() => {})
 </script>
 
 <style scoped lang='scss'>
-  .search-box {
-    --height: 35px;
-    z-index: 3;
-    height: var(--height);
-    border-radius: var(--height);
-    box-sizing: border-box;
-    display: grid;
-    grid-template-columns: auto 1fr calc(var(--height) * 1.3);
-    align-items: center;
-    background-color: var(--color-white);
-    position: relative;
-    transition: .25s linear;
-    transform-origin: right bottom;
-    margin-left: auto;
+	.search-box {
+		--height: 35px;
+		z-index: 3;
+		height: var(--height);
+		border-radius: var(--height);
+		box-sizing: border-box;
+		display: grid;
+		grid-template-columns: auto 1fr calc(var(--height) * 1.3);
+		align-items: center;
+		background-color: var(--color-white);
+		position: relative;
+		transition: .25s linear;
+		transform-origin: right bottom;
+		margin-left: auto;
 
-    .options {
-      position: absolute;
-      top: 100%;
-      left: 50%;
-      width: 100%;
-      transform: translateX(-50%) scaleY(0);
-      opacity: 0;
-      transform-origin: top center;
-      background-color: white;
-      max-height: 100px;
-      overflow-y: auto;
-      overflow-x: hidden;
-      transition: .25s linear .2s;
-
-
-      &.show {
-        transform: translateX(-50%);
-        opacity: 1;
-      }
-    }
-
-    .search {
-      grid-column: 2;
-      height: 100%;
-      width: 100%;
-      box-sizing: border-box;
-      padding-left: .25em;
-    }
+		.options {
+			position: absolute;
+			top: 100%;
+			left: 50%;
+			width: 100%;
+			transform: translateX(-50%) scaleY(0);
+			opacity: 0;
+			transform-origin: top center;
+			background-color: white;
+			max-height: 100px;
+			overflow-y: auto;
+			overflow-x: hidden;
+			transition: .25s linear .2s;
 
 
-    .tools {
-      grid-column: 1;
-      grid-row: 1;
-      box-sizing: border-box;
-      font-size: .75em;
-      padding-left: .75em;
-      padding-right: .3em;
-      border-right: 1px solid rgba(0, 0, 0, .3);
-      overflow: hidden;
+			&.show {
+				transform: translateX(-50%);
+				opacity: 1;
+			}
+		}
 
-      &:deep(span) {
-        white-space: nowrap;
-      }
-    }
+		.search {
+			grid-column: 2;
+			height: 100%;
+			width: 100%;
+			box-sizing: border-box;
+			padding-left: .25em;
+		}
 
-    .btn {
-      position: absolute;
-      right: 0;
-      width: calc(var(--height) * 1.3);
-      height: 100%;
-      background-color: var(--color-bg-btn);
-      color: white;
-      border-radius: 0 var(--height) var(--height) 0;
-    }
-  }
 
-  .search-box.blank {
-    width: var(--height);
-    opacity: .6;
-    transform: scale(.8);
+		.tools {
+			grid-column: 1;
+			grid-row: 1;
+			box-sizing: border-box;
+			font-size: .75em;
+			padding-left: .75em;
+			padding-right: .3em;
+			border-right: 1px solid rgba(0, 0, 0, .3);
+			overflow: hidden;
 
-    .btn {
-      width: var(--height);
-      border-radius: 50%;
-    }
+			&:deep(span) {
+				white-space: nowrap;
+			}
+		}
 
-    .options {
-      display: none;
-    }
+		.btn {
+			position: absolute;
+			right: 0;
+			width: calc(var(--height) * 1.3);
+			height: 100%;
+			background-color: var(--color-bg-btn);
+			color: white;
+			border-radius: 0 var(--height) var(--height) 0;
+		}
+	}
 
-    &:has(:hover, :focus-within) {
-      width: 100%;
-      opacity: 1;
-      transform: none;
+	.search-box.blank {
+		width: var(--height);
+		opacity: .6;
+		transform: scale(.8);
 
-      .btn {
-        width: calc(var(--height) * 1.3);
-        border-radius: 0 var(--height) var(--height) 0;
-      }
+		.btn {
+			width: var(--height);
+			border-radius: 50%;
+		}
 
-      .options {
-        display: block;
-      }
-    }
-  }
+		.options {
+			display: none;
+		}
+
+		&:has(:hover, :focus-within) {
+			width: 100%;
+			opacity: 1;
+			transform: none;
+
+			.btn {
+				width: calc(var(--height) * 1.3);
+				border-radius: 0 var(--height) var(--height) 0;
+			}
+
+			.options {
+				display: block;
+			}
+		}
+	}
 </style>
--- pages/Article/Article.vue
+++ pages/Article/Article.vue
@@ -1,212 +1,212 @@
 <template>
-  <view class="container">
-    <ShowMD :mdText='mdText' :showMd="showMd" @update='update'
-      :class="{'show-save':showSave,'show-download':showDownload}" @updateCoexist='b=>isCoexist=b'>
-      <template #default>
-        <SearchWrap class='search-wrap' :auto-shrink='true'></SearchWrap>
-      </template>
-      <template #header>
-        <view class="btns">
-          <navigator class="back" open-type='redirect' url="/pages/Home/Home">首页</navigator>
-          <button class="updateBtn" v-show="!isCoexist" @tap="switchView">{{showMd?'预览':'编辑'}}</button>
-          <button class="saveBtn " @tap="save"
-            v-show="showSave||showDownload">{{showSave?(main.isOnline?'上传':'保存'):showDownload?'下载':'保存'}}</button>
-          <button class="recoverBtn " @tap="recover" v-show="showSave">恢复</button>
+	<view class="container">
+		<ShowMD :mdText='mdText' :showMd="showMd" @update='update'
+			:class="{'show-save':showSave,'show-download':showDownload}" @updateCoexist='b=>isCoexist=b'>
+			<template #default>
+				<SearchWrap class='search-wrap' :auto-shrink='true'></SearchWrap>
+			</template>
+			<template #header>
+				<view class="btns">
+					<navigator class="back" open-type='redirect' url="/pages/Home/Home">首页</navigator>
+					<button class="updateBtn" v-show="!isCoexist" @tap="switchView">{{showMd?'预览':'编辑'}}</button>
+					<button class="saveBtn " @tap="save"
+						v-show="showSave||showDownload">{{showSave?(main.isOnline?'上传':'保存'):showDownload?'下载':'保存'}}</button>
+					<button class="recoverBtn " @tap="recover" v-show="showSave">恢复</button>
 
-          <picker v-if='main.artList[0]' class="pickerBtn" mode="selector" :range="main.artList" range-key="title"
-            :value='currentArtIndex' @change="currentArtIndex=$event.detail.value">
-            {{"文章:"+currentArt.title}}
-          </picker>
-          <button class="printBtn" @tap='print'>打印</button>
-        </view>
-      </template>
-    </ShowMD>
-  </view>
+					<picker v-if='main.artList[0]' class="pickerBtn" mode="selector" :range="main.artList"
+						range-key="title" :value='currentArtIndex' @change="currentArtIndex=$event.detail.value">
+						{{"文章:"+currentArt.title}}
+					</picker>
+					<button class="printBtn" @tap='print'>打印</button>
+				</view>
+			</template>
+		</ShowMD>
+	</view>
 </template>
 
 <script setup lang="ts">
-  import { ref, onMounted, watch, computed, } from "vue";
-  import { onLoad } from '@dcloudio/uni-app';
-  import { getCurd } from "@/utils/getCurd";
-  import { Article } from "@/type";
-  import useMainStore from "@/stores/useMainStore";
-  import { downloadArt } from "@/tools/dowloadArt"
-  import { useEventListener } from "@/utils/event"
-  const main = useMainStore();
-  // 当前文章下标数据的key
-  const currenteIndexKey = 'articleIndex';
-  const curdArt = getCurd<Article>('articles');
-  const mdText = ref<string>('');
-  let query : { title : string };
-  const currentArtIndex = ref<number>(0);
-  const currentArt = computed<Article>(() => main.artList[currentArtIndex.value]);
-  /**切换视图*/
-  const showMd = ref<Boolean>(false);
-  const isCoexist = ref<boolean>(true);
-  const showSave = ref<Boolean>(false);
-  const showDownload = computed<boolean>(() => main.artList[currentArtIndex.value].updateCount);
-  const keydownFns = new Map()
-  keydownFns.set('KeyA', switchView)
-  keydownFns.set('KeyS', () => showSave.value && save())
-  keydownFns.set('KeyD', () => {
-    if (showSave.value) {
-      save(); save();
-    } else { save(); }
-  })
+	import { ref, onMounted, watch, computed, } from "vue";
+	import { onLoad } from '@dcloudio/uni-app';
+	import { getCurd } from "@/utils/getCurd";
+	import { Article } from "@/type";
+	import useMainStore from "@/stores/useMainStore";
+	import { downloadArt } from "@/tools/dowloadArt"
+	import { useEventListener } from "@/utils/event"
+	const main = useMainStore();
+	// 当前文章下标数据的key
+	const currenteIndexKey = 'articleIndex';
+	const curdArt = getCurd<Article>('articles');
+	const mdText = ref<string>('');
+	let query : { title : string };
+	const currentArtIndex = ref<number>(0);
+	const currentArt = computed<Article>(() => main.artList[currentArtIndex.value]);
+	/**切换视图*/
+	const showMd = ref<Boolean>(false);
+	const isCoexist = ref<boolean>(true);
+	const showSave = ref<Boolean>(false);
+	const showDownload = computed<boolean>(() => main.artList[currentArtIndex.value].updateCount);
+	const keydownFns = new Map()
+	keydownFns.set('KeyA', switchView)
+	keydownFns.set('KeyS', () => showSave.value && save())
+	keydownFns.set('KeyD', () => {
+		if (showSave.value) {
+			save(); save();
+		} else { save(); }
+	})
 
-  function keydown(e : KeyboardEvent) {
-    let k = e.code;
-    if (!e.ctrlKey || !keydownFns.has(k)) return;
-    e.preventDefault();
-    keydownFns.get(k)();
-  }
-  useEventListener(window, 'keydown', keydown)
-  function update(text : string) {
-    mdText.value = text;
-    showSave.value = true;
-  };
-  onLoad((q) => {
-    query = q as { title : string }
-  });
-  onMounted(() => {
-    initArtIndex()
-    watch(currentArtIndex, () => {
-      uni.setStorageSync(currenteIndexKey, '' + currentArtIndex.value);
-      updateMdTextByIndex();
-    }, { immediate: true })
-  })
-  function switchView() {
-    if (isCoexist.value) return;
-    showMd.value = !showMd.value
-  }
-  async function save() {
-    let a = currentArt.value
-    a.content = mdText.value;
-    if (showSave.value) {
-      main.artList[currentArtIndex.value].content = a.content
-      if (main.isOnline) curdArt("update", a);
-      showSave.value = false
-      main.artList[currentArtIndex.value].updateCount = true
-      return
-    }
-    downloadArt(a)
-    main.artList[currentArtIndex.value].updateCount = false
+	function keydown(e : KeyboardEvent) {
+		let k = e.code;
+		if (!e.ctrlKey || !keydownFns.has(k)) return;
+		e.preventDefault();
+		keydownFns.get(k)();
+	}
+	useEventListener(window, 'keydown', keydown)
+	function update(text : string) {
+		mdText.value = text;
+		showSave.value = true;
+	};
+	onLoad((q) => {
+		query = q as { title : string }
+	});
+	onMounted(() => {
+		initArtIndex()
+		watch(currentArtIndex, () => {
+			uni.setStorageSync(currenteIndexKey, '' + currentArtIndex.value);
+			updateMdTextByIndex();
+		}, { immediate: true })
+	})
+	function switchView() {
+		if (isCoexist.value) return;
+		showMd.value = !showMd.value
+	}
+	async function save() {
+		let a = currentArt.value
+		a.content = mdText.value;
+		if (showSave.value) {
+			main.artList[currentArtIndex.value].content = a.content
+			if (main.isOnline) curdArt("update", a);
+			showSave.value = false
+			main.artList[currentArtIndex.value].updateCount = true
+			return
+		}
+		downloadArt(a)
+		main.artList[currentArtIndex.value].updateCount = false
 
-  }
+	}
 
-  function recover() {
-    uni.showModal({
-      title: '提示',
-      content: '是否恢复改动',
-      confirmText: '恢复',
-      cancelText: '不恢复',
-      success(e) {
-        if (!e.confirm) return;
-        mdText.value = currentArt.value.content;
-        showSave.value = false;
-      }
-    })
-  }
-  function initArtIndex() {
-    let i : number = -1;
-    // 跳转时
-    if (query && query.title) i = main.artList.findIndex((a : Article) => a.title == query.title);
-    // 跳转无参数时
-    if (!~i) i = +uni.getStorageSync(currenteIndexKey);
-    // 本地无存储时
-    if (!~i) i = 0;
-    // 触发watch
-    currentArtIndex.value = i
-  }
+	function recover() {
+		uni.showModal({
+			title: '提示',
+			content: '是否恢复改动',
+			confirmText: '恢复',
+			cancelText: '不恢复',
+			success(e) {
+				if (!e.confirm) return;
+				mdText.value = currentArt.value.content;
+				showSave.value = false;
+			}
+		})
+	}
+	function initArtIndex() {
+		let i : number = -1;
+		// 跳转时
+		if (query && query.title) i = main.artList.findIndex((a : Article) => a.title == query.title);
+		// 跳转无参数时
+		if (!~i) i = +uni.getStorageSync(currenteIndexKey);
+		// 本地无存储时
+		if (!~i) i = 0;
+		// 触发watch
+		currentArtIndex.value = i
+	}
 
-  async function updateMdTextByIndex() {
-    let { title, content } = main.artList[currentArtIndex.value]
-    // 获取url的检索字段
-    const param = new URLSearchParams(location.search)
-    // 设置title字段
-    param.set('title', title)
-    // 加入一条历史记录,第一个参数对象可通过新记录的history.state访问到,第二个暂时无用,第三个是用于确定新的url,?开头则只替代原来的query值,非问号开头,相对路径时(非https开头),会替代最后最后一个路径值,绝对路径时,取代全部值
-    history.pushState({ index: title }, null, "?" + param);
-    mdText.value = content
-  }
-  function print() {
-    window.print();
-  }
+	async function updateMdTextByIndex() {
+		let { title, content } = main.artList[currentArtIndex.value]
+		// 获取url的检索字段
+		const param = new URLSearchParams(location.search)
+		// 设置title字段
+		param.set('title', title)
+		// 加入一条历史记录,第一个参数对象可通过新记录的history.state访问到,第二个暂时无用,第三个是用于确定新的url,?开头则只替代原来的query值,非问号开头,相对路径时(非https开头),会替代最后最后一个路径值,绝对路径时,取代全部值
+		history.pushState({ index: title }, null, "?" + param);
+		mdText.value = content
+	}
+	function print() {
+		window.print();
+	}
 </script>
 
 <style lang="scss" scoped>
-  .container {
-    display: flex;
-    max-width: 100%;
-  }
+	.container {
+		display: flex;
+		max-width: 100%;
+	}
 
-  .show-download {
-    box-shadow: 0px 0px 40px #69aa99, 0px 0px 60px #4F8073;
-  }
+	.show-download {
+		box-shadow: 0px 0px 40px #69aa99, 0px 0px 60px #4F8073;
+	}
 
-  .show-save {
-    box-shadow: 0px 0px 40px hsl(345, 88%, 80%), 0px 0px 60px hsl(345, 88%, 80%);
-  }
+	.show-save {
+		box-shadow: 0px 0px 40px hsl(345, 88%, 80%), 0px 0px 60px hsl(345, 88%, 80%);
+	}
 
-  .btns {
-    display: flex;
-    width: 100%;
-    padding-left: 8px;
-    gap: 8px;
+	.btns {
+		display: flex;
+		width: 100%;
+		padding-left: 8px;
+		gap: 8px;
 
-    &>:not(.search-wrap) {
-      background-color: var(--color-3);
-      border-radius: 12px;
-      box-sizing: border-box;
-      display: flex;
-      align-items: center;
-      white-space: nowrap;
-      height: 32px;
-      font-size: 16px;
-      font-weight: 800;
-      text-shadow: 1px 1px 1px hsl(0, 0%, 97%);
-      color: var(--color-font-default);
-      padding: 4px 16px;
-      box-shadow: var(--shadow-btn);
-      margin: 0;
+		&>:not(.search-wrap) {
+			background-color: var(--color-3);
+			border-radius: 12px;
+			box-sizing: border-box;
+			display: flex;
+			align-items: center;
+			white-space: nowrap;
+			height: 32px;
+			font-size: 16px;
+			font-weight: 800;
+			text-shadow: 1px 1px 1px hsl(0, 0%, 97%);
+			color: var(--color-font-default);
+			padding: 4px 16px;
+			box-shadow: var(--shadow-btn);
+			margin: 0;
 
-      &:hover {
-        box-shadow: var(--shadow-btn-hover);
-      }
+			&:hover {
+				box-shadow: var(--shadow-btn-hover);
+			}
 
-      &:active {
-        box-shadow: var(--shadow-btn-active);
-      }
-    }
-  }
+			&:active {
+				box-shadow: var(--shadow-btn-active);
+			}
+		}
+	}
 
-  .search-wrap {
-    background-color: white;
-    position: fixed;
-    right: 20px;
-    top: 50%;
-    z-index: 2;
-  }
-
-
-
-  .pickerBtn:not(.a) {
-    margin-left: auto;
-  }
-
-  .recoverBtn {
-    margin-right: auto;
-  }
+	.search-wrap {
+		background-color: white;
+		position: fixed;
+		right: 20px;
+		top: 50%;
+		z-index: 2;
+	}
 
 
 
-  @media print {
-    .container>*:not(.markdown-box) {
-      display: none;
-    }
+	.pickerBtn:not(.a) {
+		margin-left: auto;
+	}
 
-    .container {
-      max-width: 100%;
-    }
-  }
+	.recoverBtn {
+		margin-right: auto;
+	}
+
+
+
+	@media print {
+		.container>*:not(.markdown-box) {
+			display: none;
+		}
+
+		.container {
+			max-width: 100%;
+		}
+	}
 </style>
